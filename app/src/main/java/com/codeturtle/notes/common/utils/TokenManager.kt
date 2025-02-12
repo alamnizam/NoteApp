68d1763917416d91358077addaa99af79fccc4e4
nalamqas <alamnizam1992@gmail.com>
2025-02-12 16:25:10 +0530

    TokenManager class creation complete, saveToken and getToken methods implemented

---
+++ app/src/main/java/com/codeturtle/notes/common/utils/TokenManager.kt
@@ -0,0 +1,24 @@
+package com.codeturtle.notes.common.utils
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import com.codeturtle.notes.common.utils.Pref.USER_TOKEN
+import kotlinx.coroutines.flow.first
+import javax.inject.Inject
+
+class TokenManager @Inject constructor(
+    private val dataStore: DataStore<Preferences>
+) {
+    suspend fun saveToken(token: String) {
+        dataStore.edit { preferences ->
+            preferences[stringPreferencesKey(USER_TOKEN)] = token
+        }
+    }
+
+    suspend fun getToken(): String? {
+        val preferences = dataStore.data.first()
+        return preferences[stringPreferencesKey(USER_TOKEN)]
+    }
+}
