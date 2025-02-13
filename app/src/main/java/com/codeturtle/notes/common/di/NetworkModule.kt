d8e28d349fd8203109e3f2084103b15a6cc6ba86
nalamqas <alamnizam1992@gmail.com>
2025-02-13 18:40:06 +0530

    Refactor NetworkModule for network operations
    NetworkModule is updated to focus on providing network-related instances, like OkHttpClient and Retrofit.
    The TokenManager is no longer provided within this module, as it is not directly involved in the network operations.

--- app/src/main/java/com/codeturtle/notes/common/di/NetworkModule.kt
+++ app/src/main/java/com/codeturtle/notes/common/di/NetworkModule.kt
@@ -1,10 +1,7 @@
 package com.codeturtle.notes.common.di
 
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
 import com.codeturtle.notes.common.constant.ServerUrlList.BASE_URL
 import com.codeturtle.notes.common.utils.AuthInterceptor
-import com.codeturtle.notes.common.utils.TokenManager
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -35,11 +32,5 @@ open class NetworkModule {
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .addInterceptor(authInterceptor)
             .build()
-    }
-
-    @Provides
-    @Singleton
-    fun provideTokenManager(dataStore: DataStore<Preferences>): TokenManager {
-        return TokenManager(dataStore)
     }
 }
