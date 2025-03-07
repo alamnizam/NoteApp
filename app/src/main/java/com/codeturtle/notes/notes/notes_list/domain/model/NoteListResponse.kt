eeef65547b79018b21516daf94cee60b023eb9f3
nalamqas <alamnizam1992@gmail.com>
2025-03-08 03:47:52 +0530

    feat: Remove note list network feature
    - Removed `NoteListApiService`, `NoteListDataModule`, `NoteListDomainModule`, `NoteListRepository`, `NoteListRepositoryImpl`, `NoteListResponse`, `NoteListResponseItem`, `NoteListScreen`, `NoteListState`, `NoteListUIEvent`, `NoteListUseCase` and `NoteListViewModel`.
    - Removed all code related to network calls for fetching and displaying a list of notes.

--- app/src/main/java/com/codeturtle/notes/notes/notes_list/domain/model/NoteListResponse.kt
+++
@@ -1,3 +0,0 @@
-package com.codeturtle.notes.notes.notes_list.domain.model
-
-class NoteListResponse : ArrayList<NoteListResponseItem>()
--- app/src/main/java/com/codeturtle/notes/notes/notes_list/domain/repository/NoteListRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.codeturtle.notes.notes.notes_list.domain.repository
-
-import com.codeturtle.notes.notes.notes_list.domain.model.NoteListResponseItem
-import retrofit2.Response
-
-interface NoteListRepository {
-    suspend fun getNoteList() : Response<List<NoteListResponseItem>>
-}
