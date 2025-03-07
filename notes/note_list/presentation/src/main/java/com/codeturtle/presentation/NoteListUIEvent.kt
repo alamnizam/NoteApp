680f2f20f05c4a87eff98405b35a8427d154d200
nalamqas <alamnizam1992@gmail.com>
2025-03-08 02:44:51 +0530

    Refactor: Move NoteListResponseItem to domain module
    - Moved `NoteListResponseItem` from the `notes_list` module to the `domain` module.
    - Updated `NoteListUIEvent` to reference the `NoteListResponseItem` from the `domain` module.

--- notes/note_list/presentation/src/main/java/com/codeturtle/presentation/NoteListUIEvent.kt
+++ notes/note_list/presentation/src/main/java/com/codeturtle/presentation/NoteListUIEvent.kt
@@ -1,6 +1,6 @@
 package com.codeturtle.presentation
 
-import com.codeturtle.notes.notes.notes_list.domain.model.NoteListResponseItem
+import com.codeturtle.notes.domain.model.NoteListResponseItem
 
 sealed class NoteListUIEvent(
     val note: NoteListResponseItem? = null
