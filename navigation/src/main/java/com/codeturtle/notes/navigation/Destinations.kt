039aa5b5a114309021a90628bd7eab0fc2bd6bd0
nalamqas <alamnizam1992@gmail.com>
2025-03-07 19:47:21 +0530

    Refactor: Remove specific navigation graphs from Destinations
    - Removed `AuthNavGraph` and `NoteNavGraph` imports and references from `Destinations.kt`.
    - Updated `Destinations` to be a generic sealed class with no specific navigation graph dependencies.

--- navigation/src/main/java/com/codeturtle/notes/navigation/Destinations.kt
+++ navigation/src/main/java/com/codeturtle/notes/navigation/Destinations.kt
@@ -1,8 +1,5 @@
 package com.codeturtle.notes.navigation
 
-import com.codeturtle.notes.authentication.navigation.AuthNavGraph
-import com.codeturtle.notes.notes.navigation.NoteNavGraph
-
 sealed class Destinations<T> (
     val route: T
 ){
