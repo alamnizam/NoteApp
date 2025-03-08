8e0657118c9749c764473c913a577b0b64c4eb85
nalamqas <alamnizam1992@gmail.com>
2025-03-08 05:47:57 +0530

    Refactor: Remove CustomTestRunner and JsonFileReader
    - Removed `CustomTestRunner` as it's no longer needed for test setup.
    - Removed `JsonFileReader` as it's not being used in the current test setup.

--- app/src/test/java/com/codeturtle/notes/util/JsonFileReader.kt
+++
@@ -1,9 +0,0 @@
-package com.codeturtle.notes.util
-
-import java.io.InputStreamReader
-
-object JsonFileReader {
-    fun getJsonContent(fileName: String): String {
-        return InputStreamReader(this.javaClass.classLoader!!.getResourceAsStream(fileName)).use { it.readText() }
-    }
-}
