3889e81dcaccff4727c57d02602cef6abf5e7e1d
nalamqas <alamnizam1992@gmail.com>
2025-03-17 19:19:06 +0530

    feat: Add theme module with basic configuration
    -   Created a new `theme` module for UI-related components.
    -   Added a basic `.gitignore` to the `theme` module to exclude the `/build` directory.
    -   Created `ExampleUnitTest` and `ExampleInstrumentedTest` for initial testing setup in the `theme` module.
    -   Updated `gradle.xml` to include the new `theme` module.
    -   Updated `settings.gradle.kts` to include the new `theme` module.
    -   Created `build.gradle.kts` with basic plugin and dependency configurations in the `theme` module.
    -   Created `proguard-rules.pro` for code shrinking and obfuscation in the `theme` module.
    -   Added an empty `AndroidManifest.xml` to the `theme` module.
    -   Created `consumer-rules.pro` for consumer-specific rules in `theme` module.

---
+++ theme/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.codeturtle.notes.theme"
+    buildFeatures{
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
