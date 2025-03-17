c139291392565badf874582cff57861a83ae86db
nalamqas <alamnizam1992@gmail.com>
2025-03-17 19:38:17 +0530

    feat: Add Google Fonts support and update dependencies
    - Added `androidx-ui-text-google-fonts` dependency to `libs.versions.toml`.
    - Updated `theme/build.gradle.kts` to include Google Fonts dependency and Compose dependencies.
    - Added dependency version for `uiTextGoogleFonts` in `libs.versions.toml`.

--- theme/build.gradle.kts
+++ theme/build.gradle.kts
@@ -12,10 +12,15 @@ android {
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    //compose
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    debugImplementation(libs.androidx.ui.tooling)
+
+    //google fonts
+    implementation(libs.androidx.ui.text.google.fonts)
 }
